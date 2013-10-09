65406f0dfe09ff8e5ee9dc0550617e06b6d2eadc
NicholasGolle <nicholasgolle@cmail.carleton.ca>
2013-10-09 16:42:23 -0400

    rororo

---
+++ lab4Pair1.java
@@ -0,0 +1,22 @@
+public class HelloWorld {
+
+    public void methodA{
+	test for first scenario
+	second trial for first scenario....
+    	//User A: Enter your code below this line (Scenario 1)
+    }
+
+    public void methodB{
+    	//User B: Enter your code below this line (Scenario 1)
+	printf("test");
+	
+    }
+
+    public void methodC{
+	scenario 2
+    	//Both users should enter code below this line (Scenario 2)
+	printf("test 2");
+	printf("no automerge");
+	scenario 2
+    }
+}
