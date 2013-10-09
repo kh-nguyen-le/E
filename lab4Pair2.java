92cf115660119c55ada71bc0a45c6c6bf8ac1f38
Aniekan Akai <aniekanakai@cmail.carleton.ca>
2013-10-09 16:45:51 -0400

    Pair 2 scenario 1

---
+++ lab4Pair2.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+
+    public void methodA{
+	test for first scenario
+	second trial for first scenario....
+    	//User A: Enter your code below this line (Scenario 1)
+    }
+
+    public void methodB{
+    	//User B: Enter your code below this line (Scenario 1)
+	printf("test");
+	
+    }
+}
