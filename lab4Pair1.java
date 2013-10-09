f6644ad133f69b1bfe7fb6546dcde4664ddd8464
YuanSun <yuansun@cmail.carleton.ca>
2013-10-09 16:00:27 -0400

    Pair 1

---
+++ lab4Pair1.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+
+    public void methodA{
+    	//User A: Enter your code below this line (Scenario 1)
+    }
+
+    public void methodA{
+    	//User B: Enter your code below this line (Scenario 1)
+    }
+
+    public void methodC{
+    	//Both users should enter code below this line (Scenario 2)
+    }
+}
