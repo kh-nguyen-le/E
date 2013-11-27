d9b52cac01efdee1f61c2ea5d1771151af6a976b
aniekan akai <aniekanakai@cmail.carleton.ca>
2013-11-27 23:21:10 +0000

    updated

--- AlarmClient.java
+++ AlarmClient.java
@@ -8,45 +8,45 @@ import java.util.logging.Logger;
 
 
 public class AlarmClient {
-	private Client client;
-	private static java.net.InetAddress gateway;
-	
-	public AlarmClient()  {
-		client = new Client();
-		Network.register(client);
-		AlarmListener listener = new AlarmListener();
-                listener.init(client);
-		client.addListener(listener);
-		
-		do {
-			gateway =client.discoverHost(Network.port, 60000);
-		}while (gateway==null);	
-                client.start();
-		
-		try {
-                    Log.set(Log.LEVEL_TRACE);
-                    client.connect(60000, gateway, Network.port, Network.port);
-                    client.setTimeout(0);
-                    client.setKeepAliveTCP(0);
-                    client.setKeepAliveUDP(0);
-                     
-		}catch(IOException e){
-			e.printStackTrace();
-			client.stop();
-		}
-                while(!client.isConnected()){
-                    try {
-                        client.reconnect(60000);
-                    } catch (IOException ex) {
-                        ex.printStackTrace();
-                    }
-                }
-	}
-	
-	public static void main(String[] args) throws InterruptedException  {
-		new AlarmClient();
-                while(true){
-        		Thread.sleep(999999);
-                }
-         }
+        private Client client;
+        private static java.net.InetAddress gateway;
+        
+        public AlarmClient()  {
+                client = new Client();
+                Network.register(client);
+                AlarmListener listener = new AlarmListener();
+                listener.init(client);
+                client.addListener(listener);
+                
+                do {
+                        gateway =client.discoverHost(Network.port, 60000);
+                }while (gateway==null);        
+                client.start();
+                
+                try {
+                    Log.set(Log.LEVEL_TRACE);
+                    client.connect(60000, gateway, Network.port, Network.port);
+                    client.setTimeout(0);
+                    client.setKeepAliveTCP(0);
+                    client.setKeepAliveUDP(0);
+                     
+                }catch(IOException e){
+                        e.printStackTrace();
+                        client.stop();
+                }
+                while(!client.isConnected()){
+                    try {
+                        client.reconnect(60000);
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+        }
+        
+        public static void main(String[] args) throws InterruptedException  {
+                new AlarmClient();
+                while(true){
+                        Thread.sleep(999999);
+                }
+         }
 }
