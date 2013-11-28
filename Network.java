41d87f1c2a9152fc0d8c44097e94b2d0e11ce349
Aniekan Akai <aniekanakai@cmail.carleton.ca>
2013-11-28 16:53:33 -0500

    Network.java added

---
+++ Network.java
@@ -0,0 +1,25 @@
+package securitySystem;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryonet.EndPoint;
+
+public class Network {
+	static public final int port = 27960;
+	
+	static public void register (EndPoint endPoint) {
+                Kryo kryo = endPoint.getKryo();
+                kryo.register(MessagePacket.class);
+                kryo.register(AlertPacket.class);
+                kryo.register(MotorPacket.class);
+                kryo.register(AuthenticationPacket.class);
+                kryo.register(HandshakePacket.class);
+                kryo.register(SettingsPacket.class);
+	}
+	
+        public static class MessagePacket { public String message; }
+	public static class AlertPacket { public boolean alarmOn  = false; }
+	public static class MotorPacket { public boolean direction; }
+	public static class AuthenticationPacket { }
+	public static class HandshakePacket {public boolean success = false; }
+	public static class SettingsPacket { }
+}
