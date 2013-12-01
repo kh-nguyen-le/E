6382b02ce5743d6a519cae45a222f96288ca08c6
Aniekan Akai <aniekanakai@cmail.carleton.ca>
2013-12-01 17:25:45 -0500

    moved stream to listner

--- CameraClient.java
+++ CameraClient.java
@@ -24,15 +24,6 @@ public class CameraClient {
         
         public CameraClient() {
                 client = new Client();
-                // Video stream from the  PI_CAMERA with the use of command line command
-              try{
-                 System.out.println("Begin stream");
-                 java.lang.Runtime rt = Runtime.getRuntime();
-                 java.lang.Process pr = rt.exec("raspivid -o - -w 920 -h 540 -t 9999999 |cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/}' :demux=h264");
-				
-              }catch(IOException e){
-              	e.printStackTrace();
-              }
                // create gpio controller
 		gpio = GpioFactory.getInstance();
                 Network.register(client);
