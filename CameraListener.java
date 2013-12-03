a401125f9034f39a1701b88c6e090eb8c3cd3a77
Khoi Le <johnle@cmail.carleton.ca>
2013-12-03 16:47:49 -0500

    exploring ProcessBuilder solution

--- CameraListener.java
+++ CameraListener.java
@@ -80,13 +80,7 @@ class StreamingThread extends Thread
     public StreamingThread(){}
     
     public void run(){
-                try{
                     //System.out.println("Begin stream");
-                    java.lang.Runtime rt = Runtime.getRuntime();
-                    java.lang.Process pr = rt.exec(new String[]{"bash", "-c","raspivid -o - -w 920 -h 540 -t 9999999 |cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/}' :demux=h264"});//"python stream.py");
-				
-                }catch(IOException e){
-                    e.printStackTrace();
-                }
+                    ProcessBuilder p = new ProcessBuilder("bash", "-c","raspivid -o - -w 920 -h 540 -t 9999999 |cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/}' :demux=h264");//"python stream.py");
     }
 }
